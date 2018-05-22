58e00fa1c55b5def92223922f932e9c82b8e0d9b
Mike Gomez <mikegomez21@hotmail.com>
2018-05-21 20:19:48 -0500

    friend finder app

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+       "name":"Jimmy the Carrey",
+       "photo":"http://thinkup.me/wp-content/uploads/2017/11/Jim_Carrey-1-e1510001639348.jpg",
+       "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+       "name":"Smarty McFly",
+       "photo":"http://ostrava.educanet.cz/files/www/ostrava-educanet.cz_anglictina/ostrava-educanet.cz/anglictina/ostrava-educanet.cz/www_anglictina/images/stories/2_rocnik/AlbertEinstein.jpg",
+       "scores":[4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+       "name":"Marilyn HotPants",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlE8bWQCdKBqLAKazZh7GwC9ad-si4H8x8MuZWd10PP0vLAheR",
+       "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+       "name":"Demis",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHBJriZjJE10x62k1YMPzPx0Qi9qdQUy0fjo5rgzYoDNRGkTVA",
+       "scores":[3,3,4,2,2,1,3,2,2,3]
+    },
+     {
+        "name":"Hefter the man",
+        "photo":"http://s1.ibtimes.com/sites/www.ibtimes.com/files/styles/lg/public/2017/12/28/dead-celebrities-2017-quotes-famous-people-who-died-year.jpg",
+        "scores":[4,3,4,1,5,2,5,3,1,4]
+     },
+       {
+       "name":"Ari Grande",
+       "photo":"https://www.proudstories.com/wp-content/uploads/2017/02/Famous-People-from-Florida-Ariana-Grande.jpeg",
+       "scores":[3,3,4,1,4,3,5,3,5,4]
+    },
+    {
+        "name":"Opera",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfmHMmAOH7hSj-iKeEKNw-HqdKOhvyHMQGVCu7yN-8AruXgHkI",
+        "scores":[4,4,2,3,2,2,3,2,4,5]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
